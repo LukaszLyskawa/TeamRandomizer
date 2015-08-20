53c0a88d8d1ed6f2f5b3a714b4a24ed4d403f065
Lukasz Lyskawa <lyskawalukasz@gmail.com>
2015-08-20 20:12:53 +0200

    Add filehelper project

---
+++ TeamRandomizer/FileHelper/TextFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHelper
+{
+    public class TextFile
+    {
+        public void WriteFile<T>(IEnumerable<T> objectsToWrite,IEnumerable<FieldInfo>filesToWrite,string path,int teamSize=5)
+        {
+            
+        }
+    }
+}
